aa2fc879adac5fadb775fa4d8c0008ca30cb227d
jengyuwang <jengyu@yahoo.com>
2018-09-23 13:25:31 -0700

    Remove debug code

--- DC_Motor_PWM/main.cpp
+++ DC_Motor_PWM/main.cpp
@@ -5,20 +5,17 @@
 
 DigitalIn  switchInput(p17, PullNone);
 PwmOut MOSFET(p21);
-Serial pc(USBTX, USBRX);
 
 int main() 
 {
 	MOSFET.period_us(500);
 	MOSFET = 0.0;
-
     while(1)
     {
     	for (int i = 0; i <= 10; i++)
     	{
     		if (switchInput == 1)
     		{
-    			pc.printf("switchInput = %d\t DutyCycle = %.1f\r\n", switchInput.read(), i/10.);
         		MOSFET = i/10.;
         		wait(2.0);
     		}
@@ -29,7 +26,6 @@ int main()
    		{
     		if (switchInput == 1)
     		{
-    			pc.printf("switchInput = %d\t DutyCycle = %.1f\r\n", switchInput.read(), (10-i)/10.);
        			MOSFET = (10-i)/10.;
        			wait(2.0);
     		}
